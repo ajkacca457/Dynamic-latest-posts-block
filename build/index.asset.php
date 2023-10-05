0c80fd2495d9d5306487d51443f676313753656e
Avijit Karmaker <ajkacca@gmail.com>
2023-10-05 23:24:44 +0300

    update block width and functionality

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'cd53796bae6d4f64d615');
+<?php return array('dependencies' => array('moment', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b3233f33ace430973fd0');
